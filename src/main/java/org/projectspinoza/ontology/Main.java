8f3d785c07dd101f55f42b1ec8ded94a93631d16
Muhammad Sadiq <m.sadiq.khan92@gmail.com>
2015-11-12 11:13:07 +0500

    initial commit

---
+++ src/main/java/org/projectspinoza/ontology/Main.java
@@ -0,0 +1,18 @@
+package org.projectspinoza.ontology;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+	private static Logger log = LogManager.getRootLogger();
+    public static void main( String[] args )
+    {
+        log.info( "Initializing ontologies!" );
+        
+        log.info( "Done" );
+    }
+}
