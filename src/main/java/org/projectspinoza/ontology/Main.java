147bd8bb1275a3b654a2fa7c512b6e46b1d9e854
uMklami <m.aamir.it@gmail.com>
2015-11-27 15:36:50 +0500

    commented lines removed

--- src/main/java/org/projectspinoza/ontology/Main.java
+++ src/main/java/org/projectspinoza/ontology/Main.java
@@ -3,10 +3,6 @@ package org.projectspinoza.ontology;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * Hello world!
- *
- */
 public class Main {
 	private static Logger log = LogManager.getRootLogger();
 
